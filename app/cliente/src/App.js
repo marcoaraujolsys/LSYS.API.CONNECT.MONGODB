669d2baff4df4e1cb89fcaf88684d019771b26d4
Marco Araujo <marcoaraujolsys@hotmail.com>
2021-04-24 08:47:25 -0300

    Refatoração e redução de arquivos

--- app/cliente/src/App.js
+++ app/cliente/src/App.js
@@ -1,29 +1,103 @@
+
 import './App.css';
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Input , Button } from '@material-ui/core';
+import { DataGrid } from '@material-ui/data-grid';
 
-export default class App1 extends React.Component {
+const columns = [
+  { field: 'nome', headerName: 'NOME DO CLIENTE', width: 250, sortable: true },
+  { field: 'valor', headerName: 'VALOR', width: 250, sortable: true },
+  { field: 'data', headerName: 'DESDE', width: 250,  sortable: true }
+];
+
+export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      filtro: '',
+      error: null,
+      isLoaded: false,
+      rowsDB: [],
+      rows:[],
+      busca: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({busca: event.target.value});
+  }
+
+
+  componentDidMount() {
+
+    //Carrega dados do banco MongoDB
+    fetch("http://localhost:3001/cliente")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+              isLoaded: true,
+              rowsDB: this.formatarDados(result),
+              rows: this.formatarDados(result)
+            } );
+        },
+
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  //Altera o nome da chave "_id" do mongoDB para "id" para datagrid
+  formatarDados(rec) {
+    return rec.map(({_id, ...rest}) => ({...rest, id: _id}));
+  }
+
+  RecarregarTabela() {
+
+    if (this.state.busca === '') { 
+      this.setState({rows: this.state.rowsDB});
+
+    } else {
+
+      const recFiltro = this.state.rowsDB.filter( ( rec ) => {
+        return  rec.nome.toLowerCase().includes(this.state.busca.toLowerCase()) ;
+      });
+
+      this.setState({rows: recFiltro});
+
+    }
+  }
 
   render() {
- 
+    //Se ainda não carregou os dados do banco, nao monta tela.
+    if (!this.state.isLoaded) {
+      return null;
+    } else {
+
       return (
-        <Route>
         <div className="App">
           <header className="App-header">
-              TOTVS - Test FullStack
+              NODE - MONGODB
           </header>
 
-          <header className="App-header-cliente">
-              Cadastro de Clientes Inadimplentes
-          </header>
-         
-            <span>Identifique-se para acessar o controle de inadimplentes</span>
-            <div><Link to='/cliente'>SOU ANALISTA FINANCEIRO</Link></div>
-            <div><Link to='#'>SOU ANALISTA DE NEGÓCIOS</Link></div>
+          <span>Filtrar Cliente: <Input value={this.state.value} onChange={this.handleChange}/></span>
+          <Button onClick={this.RecarregarTabela.bind(this)}>Buscar</Button>
           
+          <div style={{ height: 400, width: '100%' }}>
+            <DataGrid rows={this.state.rows} columns={columns} />
+          </div>
         </div>
-        </Route>
     );
    }
 
   }
+}
+
+
--- app/cliente/src/index.js
+++ app/cliente/src/index.js
@@ -3,19 +3,11 @@
 import React from 'react';
 
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import Cliente from './listaCliente';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Switch>
-            <Route path="/" exact={true} component={App} />
-            <Route path="/cliente" component={Cliente} />
-        </Switch>
-    </BrowserRouter>
-  </React.StrictMode>,
+  <App/>,
   document.getElementById('root')
 );
